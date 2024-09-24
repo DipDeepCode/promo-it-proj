50a8d3892d797359557de959640f1fb04eeab292
Sergey Kovalev <dipdeepcode@mail.ru>
2024-09-24 21:28:51 +0300

    feat :: api-gateway is configured

--- api-gateway/src/test/java/ru/ddc/apigateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/ru/ddc/apigateway/ApiGatewayApplicationTests.java
@@ -2,12 +2,16 @@ package ru.ddc.apigateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
+    @MockBean
+    private JwtDecoder jwtDecoder;
+
     @Test
     void contextLoads() {
     }
-
 }
