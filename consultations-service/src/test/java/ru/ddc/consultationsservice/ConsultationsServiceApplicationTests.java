add0a72d46a96bc7eef81cdf3fe0a5f69b56e7b1
Sergey Kovalev <dipdeepcode@mail.ru>
2024-09-24 21:20:42 +0300

    feat :: init commit

---
+++ consultations-service/src/test/java/ru/ddc/consultationsservice/ConsultationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddc.consultationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
