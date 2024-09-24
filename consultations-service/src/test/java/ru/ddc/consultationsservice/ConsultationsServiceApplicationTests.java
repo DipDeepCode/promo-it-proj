d694a1e6e03b2027e23718e87dfbbb6ba9230107
Sergey Kovalev <dipdeepcode@mail.ru>
2024-09-24 21:37:33 +0300

    feat :: consultations-service is configured

--- consultations-service/src/test/java/ru/ddc/consultationsservice/ConsultationsServiceApplicationTests.java
+++ consultations-service/src/test/java/ru/ddc/consultationsservice/ConsultationsServiceApplicationTests.java
@@ -2,12 +2,16 @@ package ru.ddc.consultationsservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 
 @SpringBootTest
 class ConsultationsServiceApplicationTests {
 
+    @MockBean
+    private JwtDecoder jwtDecoder;
+
     @Test
     void contextLoads() {
     }
-
 }
